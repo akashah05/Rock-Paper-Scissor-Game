d97eff86ee266515b7f1eb31ec00a87e02ade6bf
Akashah <akashahiscoding@gmail.com>
2025-01-14 16:27:37 +0500

    Add Ui to rock-paper-scissor
    Make a few changes in existing code to compatible
    with ui

--- script.js
+++ script.js
@@ -1,67 +1,92 @@
+const rockBtn = document.querySelector("#btn-one");
+const paperBtn = document.querySelector("#btn-two");
+const scissorBtn = document.querySelector("#btn-three");
+
+const resultDisplay = document.querySelector("#result");
+const computerScoreDisplay = document.querySelector("#c-score");
+const humanScoreDisplay = document.querySelector("#h-score");
+const tieRoundsDisplay = document.querySelector("#t-rounds")
+
 const getComputerChoice = () => {
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log("Random:", randomNumber);
-  if (randomNumber === 1) {
-    return "Rock";
-  } else if (randomNumber === 2) {
-    return "Paper";
-  } else if (randomNumber === 3) {
-    return "Scissor";
-  }
+    let options = ["Rock", "Paper", "Scissor"];
+    let randomNumber = Math.floor(Math.random() * options.length);
+    return options[randomNumber];
 };
 
-const getHumanChoice = () => {
-  let userChoice = parseInt(
-    prompt("Enter your Choice:\n1-For Rock \n2-For Paper \n3-For Scissor")
-  );
-  if (userChoice === 1) {
-    return "Rock";
-  } else if (userChoice === 2) {
-    return "Paper";
-  } else if (userChoice === 3) {
-    return "Scissor";
-  } else {
-    return "Wrong Input!";
-  }
-};
 
 let humanScore = 0;
 let computerScore = 0;
+let tieRounds = 0
+let roundCount = 0;
 
 const playRound = (computerChoice, humanChoice) => {
-  if (computerChoice === humanChoice) {
-    return `Match Tie!, \nComputer Select: ${computerChoice} \nYou Select: ${humanChoice}`;
-  } else if (
-    (humanChoice === "Paper" && computerChoice === "Rock") ||
-    (humanChoice === "Rock" && computerChoice === "Scissor") ||
-    (humanChoice === "Scissor" && computerChoice === "Paper")
-  ) {
-    humanScore++;
-    return `Congratulation, You Won. \nComputer Select: ${computerChoice} \nYou Select: ${humanChoice}`;
-  } else if (
-    (computerChoice === "Paper" && humanChoice === "Rock") ||
-    (computerChoice === "Rock" && humanChoice === "Scissor") ||
-    (computerChoice === "Scissor" && humanChoice === "Paper")
-  ) {
-    computerScore++;
-    return `Computer Win!, \nComputer Select: ${computerChoice} \nYou Select: ${humanChoice}`;
-  } else {
-    return `Wrong Selection, \nComputer Select:${computerChoice} \nYou Select: ${humanChoice}`;
-  }
+    if (computerChoice === humanChoice) {
+        tieRounds++;
+        return `Match Tie!, \nComputer Select: ${computerChoice} \nYou Select: ${humanChoice}`;
+
+    } else if (
+        (humanChoice === "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Rock" && computerChoice === "Scissor") ||
+        (humanChoice === "Scissor" && computerChoice === "Paper")
+    ) {
+        humanScore++;
+        return `Congratulation, You Won. \nComputer Select: ${computerChoice} \nYou Select: ${humanChoice}`;
+    } else if (
+        (computerChoice === "Paper" && humanChoice === "Rock") ||
+        (computerChoice === "Rock" && humanChoice === "Scissor") ||
+        (computerChoice === "Scissor" && humanChoice === "Paper")
+    ) {
+        computerScore++;
+        return `Computer Win!, \nComputer Select: ${computerChoice} \nYou Select: ${humanChoice}`;
+    } else {
+        return `Wrong Selection, \nComputer Select:${computerChoice} \nYou Select: ${humanChoice}`;
+    }
 };
 
-const playGame = () => {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(getComputerChoice(), getHumanChoice()));
-  }
+const displayFinalResult = () => {
+    if (humanScore > computerScore) {
+        resultDisplay.textContent = `Game Over! You Won the Game! 🏆`;
+    } else if (computerScore > humanScore) {
+        resultDisplay.textContent = `Game Over! Computer Won the Game! 🤖`;
+    } else {
+        resultDisplay.textContent = `Game Over! It's a Tie! 🤝`;
+    }
 
-  if (humanScore > computerScore) {
-    return `You Won, \nYour Score: ${humanScore} \nComputer Score: ${computerScore}`;
-  } else if (computerScore > humanScore) {
-    return `Computer Won, \nYour Score: ${humanScore} \nComputer Score: ${computerScore}`;
-  } else {
-    return `Match Tie! Both Score ${humanScore} - ${computerScore}`;
-  }
+    const refreshMsg = document.createElement("h4");
+    refreshMsg.textContent = "Refresh the page, To play Another Series 🔄";
+    document.body.appendChild(refreshMsg);
 };
 
-console.log(playGame());
+
+const getHumanChoice = (humanChoice) => {
+
+    if (roundCount >= 5) return;
+
+    const computerChoice = getComputerChoice();
+    const result = playRound(computerChoice, humanChoice);
+
+    resultDisplay.textContent = result;
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+    tieRoundsDisplay.textContent = tieRounds;
+
+    roundCount++;
+    console.log(roundCount);
+
+    if (roundCount === 5) {
+        displayFinalResult();
+    };
+};
+
+rockBtn.addEventListener("click", () => {
+    getHumanChoice("Rock");
+});
+
+paperBtn.addEventListener("click", () => {
+    getHumanChoice("Paper");
+});
+
+scissorBtn.addEventListener("click", () => {
+    getHumanChoice("Scissor");
+});
+
